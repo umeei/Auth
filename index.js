347b7ee9ef84af221907d2c496eaf29f1eb76684
umair <umairjutt2025@gmail.com>
2023-09-20 17:23:54 +0500

    adding ejss

--- index.js
+++ index.js
@@ -4,8 +4,17 @@ const app= express();
 var port = process.env.PORT || 3000;
 const mongoose = require('mongoose');
 app.use(express.json());
-
 app.use('/api/admin', Adminroutes)
+
+
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname+"/public"))
+
+
+app.get('/admin_sign_in', function(req,res){
+    res.render(__dirname+"./routes/Admin")
+
+})
 
 
 mongoose.connect("mongodb+srv://umairjutt2025:umairjutt2025@umair-cluster.oducycs.mongodb.net/adminn?retryWrites=true&w=majority").then((res)=>{
